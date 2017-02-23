5eee86f32e6096bdd9538b69af28c673657e9f0a
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-02-22 16:49:03 -0800

    fixed some errors, class works but the functions still need to be written

--- space.py
+++ space.py
@@ -1,15 +1,10 @@
 from spaceGraph import *
-
-spaceList = ['red',
-            'green',
-            'blue',
-            'yellow',
-            'white']
+from PIL import Image, ImageDraw
 
 #this is a modified implementation of the cSpace class that is customized for tensorflow
 class space(cSpace):
-    def __init__(name, childList, parentSpace = None):
-        cSpace. __init__(name, parentSpace)
+    def __init__(self, name, childList, parentSpace = None):
+        super().__init__(name, parentSpace)
         # this is the list which has all the children
         # this is needed because we need indices, and the original children dictionary in
         # cSpace class does not provide any indices
@@ -21,20 +16,21 @@ class space(cSpace):
         # this is a list of points, one corresponding to one wall, the orientation of the wall is
         # determined by the orientation of the space that it is dividing
         self.wallPos = list()
-        self.addChildren(childList)
+        # adding a new space as a child for every name in the childList
+        self.addChildren([cSpace(cName) for cName in self.cL])
     
     # this function will populate the ptList randomly
     # write this func and call it at the instance creation
-    def populatePts():
+    def populatePts(self):
         return
 
     # this function will calculate the positions of the walls
     # write this func and call it in a loop while creating the dataset
-    def makewalls():
+    def makewalls(self):
         return
         
     #returns the indices of the two spaces corresponding to the connection number
-    def getConSpaces(num):
+    def getConSpaces(self, num):
         numSpace = len(self.cL)
         possibleConnections = numSpace*(numSpace-1)/2
         if num > possibleConnections - 1:
@@ -61,7 +57,7 @@ class space(cSpace):
         return [s1,s2]
 
     #returns the index of te connection between the two spaces, whose indices are supplied as parameters.
-    def getConIndex(sA,sB):
+    def getConIndex(self, sA,sB):
         numSpace = len(self.cL)
         if sA >= numSpace or sB >= numSpace:
             print('Invalid space indices')
@@ -80,3 +76,18 @@ class space(cSpace):
         
         ans = l + s2 - s1 -1
         return ans
+
+# the main logic begins here
+# this is the space list for the dataset
+spaceList = ['red',
+            'green',
+            'blue',
+            'yellow',
+            'white']
+
+sample = space('sample', spaceList, None)
+
+# for i in range(dataNum):
+    # populate sample with pts
+    # make walls
+    # export the training example
