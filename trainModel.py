4518966461855bdbb88c616c895b4fb0a4d598aa
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-03-26 14:57:14 -0700

    udpated the batch size and training cycles to suit the smaller model

--- trainModel.py
+++ trainModel.py
@@ -17,7 +17,7 @@ with tf.Session() as sess:
     # loadModel(sess, model_save_path[0])
     # loadModel(sess, model_save_path[1])
 
-    cycles = 1600000
+    cycles = 640000
     testStep = 500
     saveStep = 2000
     log_step = 10
