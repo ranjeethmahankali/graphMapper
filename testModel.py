f9e0e7e12f9f849f30b36258ac436831849beb5c
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-04-18 21:30:22 -0700

    testing the model

--- testModel.py
+++ testModel.py
@@ -1,33 +1,27 @@
-from model import *
+from inceptionModel import *
 from ops import *
 
-image, target, keep_prob = getPlaceHolders()
-vector = interpret(image, keep_prob)
-optim, lossVal = getOptimStep(vector, target)
-graph = getGraph(vector)
+bottleneck, target, keep_prob = getPlaceHolders()
+vector, graph = interpret(bottleneck, keep_prob)
+optim, lossVal = getOptimStep(vector, graph, target)
 accuracy = accuracy(graph, target)
 
-data = dataset('data/')
+data = dataset('inception_data/')
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    loadModel(sess, model_save_path[0])
+    loadModel(sess, model_save_path[1])
     # loadModel(sess, model_save_path[1])
 
     # testBatch = data.test_batch(21)
-    testBatch = data.test_batch(5)
-    acc, lval, graph_out, vec = sess.run([accuracy, lossVal, graph, vector], feed_dict={
-        image: testBatch[0],
-        target: testBatch[1],
-        keep_prob:1.0
-    })
+    for i in range(10000//50):
+        testBatch = data.test_batch(50)
+        acc, lval, graph_out, vec = sess.run([accuracy, lossVal, graph, vector], feed_dict={
+            bottleneck: testBatch[0],
+            target: testBatch[1],
+            keep_prob:1.0
+        })
+        
+        g_sum = int(np.sum(graph_out))
+        t_sum = int(np.sum(testBatch[1]))
 
-    # print(vec)
-
-    g_sum = int(np.sum(graph_out))
-    t_sum = int(np.sum(testBatch[1]))
-
-    i = 3
-    print(vec[i])
-    print(testBatch[1][i])
-    print(graph_out[i].astype(np.uint8))
-    print('Acc: %.2f; L: %.2f; Sums: %s/%s%s'%(acc, lval,g_sum,t_sum,' '*40))
+        print('Acc: %.2f; L: %.2f; Sums: %s/%s%s'%(acc, lval,g_sum,t_sum,' '*40))
