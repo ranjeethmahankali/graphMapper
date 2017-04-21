e877b83174e4e8c81525a2538e57c6b5e416fadc
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-04-21 01:59:16 -0700

    ready to test the model

--- testModel.py
+++ testModel.py
@@ -1,27 +1,39 @@
-from inceptionModel import *
+import inceptionPrep as ip
 from ops import *
+from spaceGraph_ext import *
+
+fileName = 'results/test.png'
+img_arr = prepareImage(Image.open(fileName), normalize=False)
+
+bottleneck_val = ip.run_bottleneck_on_image(ip.sess_inception, 
+                                                img_arr, 
+                                                ip.jpeg_data_tensor, 
+                                                ip.bottleneck_tensor)
+
+bottleneck_val = np.expand_dims(bottleneck_val, axis=0)
+
+ip.sess_inception.close()
+
+from inceptionModel import *
 
 bottleneck, target, keep_prob = getPlaceHolders()
 vector, graph = interpret(bottleneck, keep_prob)
 optim, lossVal = getOptimStep(vector, graph, target)
 accuracy = accuracy(graph, target)
 
-data = dataset('inception_data/')
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    loadModel(sess, model_save_path[1])
     # loadModel(sess, model_save_path[1])
+    flat_graph_list = sess.run(graph, feed_dict={
+        bottleneck: bottleneck_val,
+        keep_prob:1.0
+    })
 
-    # testBatch = data.test_batch(21)
-    for i in range(10000//50):
-        testBatch = data.test_batch(50)
-        acc, lval, graph_out, vec = sess.run([accuracy, lossVal, graph, vector], feed_dict={
-            bottleneck: testBatch[0],
-            target: testBatch[1],
-            keep_prob:1.0
-        })
+    flatGraph = flat_graph_list[0]
+    print(flatGraph)
+    sampleSpace = space('sample', nameList, default_coords)
+    for i in range(len(flatGraph)):
+        if flatGraph[i]==0:continue
         
-        g_sum = int(np.sum(graph_out))
-        t_sum = int(np.sum(testBatch[1]))
-
-        print('Acc: %.2f; L: %.2f; Sums: %s/%s%s'%(acc, lval,g_sum,t_sum,' '*40))
+        a,b = sampleSpace.getConSpaces(i)
+        print(nameList[a], nameList[b])
