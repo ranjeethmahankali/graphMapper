aa5191fe3f12c0e7b9f2bbf587667398b6cea412
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-04-21 02:14:46 -0700

    testing multiple files at a time

--- testModel.py
+++ testModel.py
@@ -2,15 +2,19 @@ import inceptionPrep as ip
 from ops import *
 from spaceGraph_ext import *
 
-fileName = 'results/test.png'
-img_arr = prepareImage(Image.open(fileName), normalize=False)
+bottleneck_vals = []
 
-bottleneck_val = ip.run_bottleneck_on_image(ip.sess_inception, 
-                                                img_arr, 
-                                                ip.jpeg_data_tensor, 
-                                                ip.bottleneck_tensor)
+for i in range(10):
+    fileName = 'results/%s.png'%i
+    img_arr = prepareImage(Image.open(fileName), normalize=False)
 
-bottleneck_val = np.expand_dims(bottleneck_val, axis=0)
+    bottleneck_val = ip.run_bottleneck_on_image(ip.sess_inception, 
+                                                    img_arr, 
+                                                    ip.jpeg_data_tensor, 
+                                                    ip.bottleneck_tensor)
+
+    # bottleneck_val = np.expand_dims(bottleneck_val, axis=0)
+    bottleneck_vals.append(bottleneck_val)
 
 ip.sess_inception.close()
 
@@ -25,7 +29,7 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     loadModel(sess, model_save_path[1])
     flat_graph_list = sess.run(graph, feed_dict={
-        bottleneck: bottleneck_val,
+        bottleneck: bottleneck_vals,
         keep_prob:1.0
     })
 
